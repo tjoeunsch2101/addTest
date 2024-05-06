0c05176f95b5b90660df2f5444355ff1b6829e62
lew <bit4math@gmail.com>
2024-05-06 18:24:01 +0900

    first commit

---
+++ src/main/java/com/tjoeun/shop/AddTestApplication.java
@@ -0,0 +1,13 @@
+package com.tjoeun.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddTestApplication.class, args);
+	}
+
+}
